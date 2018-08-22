0c44ffb6b33c5ba5184738a8b00d6216228ab2f6
Adelaide Yoder <adelaide.yoder@gmail.com>
2018-08-22 15:43:57 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<(string product, double amount, int quantity)> transactions = new List<(string, double, int)>();
+
+            (string product, double amount, int quantity) hammer = ("Hammer", 19.99, 25);
+            (string product, double amount, int quantity) nails = ("Nails", 7.99, 100);
+            (string product, double amount, int quantity) nailGun = ("Nail Gun", 189.99, 1);
+            (string product, double amount, int quantity) bandaids = ("Bandaids", 6.72, 250);
+            (string product, double amount, int quantity) staples = ("Staples", 3.98, 50);
+
+            foreach ((string product, double amount, int quantity) t in transactions)
+            {
+                // Logic goes here to look up quantity and amount in each transaction
+                t.quantity
+            }
+
+        }
+    }
+}
